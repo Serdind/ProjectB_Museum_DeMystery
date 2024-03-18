f3feb7c5bbb51c1567c15e02c488becdc32e1e35
Serdind <1055241@hr.nl>
2024-03-18 15:52:48 +0100

    Changed int to long

--- ProjectB_Museum_DeMystery/Visitor.cs
+++ ProjectB_Museum_DeMystery/Visitor.cs
@@ -1,12 +1,12 @@
 class Visitor
 {
-    public int Code;
+    public long Code;
     public string Name;
     public string Email;
-    public int Phonenumber;
+    public long Phonenumber;
     public GuidedTour guidedTour;
 
-    public Visitor(string name, string email, int phonenumber)
+    public Visitor(string name, string email, long phonenumber)
     {
         Name = name;
         Email = email;
