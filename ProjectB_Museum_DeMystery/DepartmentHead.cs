85579cfed34887bdcd249e6e64996a56ad1f3199
Wishant010 <wishantbhajan@outlook.com>
2024-04-17 15:45:20 +0200

    Departmenthead changes

--- ProjectB_Museum_DeMystery/DepartmentHead.cs
+++ ProjectB_Museum_DeMystery/DepartmentHead.cs
@@ -1,9 +1,11 @@
 class DepartmentHead : Person
 {
+    private static int lastId = 1;
+    public int Id;
     public string Name;
-
     public DepartmentHead(string name, string qr) : base(qr)
     {
+        Id = lastId++;
         Name = name;
     }
 }
