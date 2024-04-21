3b0827208f303ba173d4ab08f7671e14f89a3cc8
jaydaqg <1075587@hr.nl>
2024-04-21 13:26:30 +0200

    Program changes

--- ProjectB_Museum_DeMystery/Program.cs
+++ ProjectB_Museum_DeMystery/Program.cs
@@ -12,106 +12,7 @@ class Program
         Tours.AddAdminToJSON();
 
         Tours.AddGuideToJSON();
-        
-
-        string connectionString = "Data Source=MyDatabase.db";
-
-        using (var connection = new SqliteConnection(connectionString))
-        {
-            connection.Open();
-
-            string createVisitorInTourTableCommand = @"
-                CREATE TABLE IF NOT EXISTS VisitorInTour (
-                    Id_Visitor INTEGER,
-                    Id_Tour INTEGER,
-                    Date TEXT NOT NULL,
-                    PRIMARY KEY (Id_Visitor, Id_Tour)
-                );";
-
-            string createVisitorTableCommand = @"
-                CREATE TABLE IF NOT EXISTS Visitors (
-                    Id INTEGER,
-                    QR TEXT NOT NULL,
-                    PRIMARY KEY (ID)
-                );";
-            
-            string createDepartmentHeadTableCommand = @"
-                CREATE TABLE IF NOT EXISTS DepartmentHead (
-                    Id INTEGER,
-                    Name TEXT NOT NULL,
-                    QR TEXT NOT NULL,
-                    PRIMARY KEY (ID)
-                );";
-            
-            string createGuideTableCommand = @"
-                CREATE TABLE IF NOT EXISTS Guide (
-                    Id INTEGER,
-                    Name TEXT NOT NULL,
-                    QR TEXT NOT NULL,
-                    PRIMARY KEY (ID)
-                );";
-            
-            string createGuideInTourTableCommand = @"
-                CREATE TABLE IF NOT EXISTS GuideInTour (
-                    Id_Guide INTEGER,
-                    Id_Tour INTEGER,
-                    Date TEXT NOT NULL,
-                    PRIMARY KEY (Id_Guide, Id_Tour)
-                );";
-
-            using (var createTable = new SqliteCommand(createVisitorInTourTableCommand, connection))
-            {
-                createTable.ExecuteNonQuery();
-            }
-
-            using (var createTable = new SqliteCommand(createVisitorTableCommand, connection))
-            {
-                createTable.ExecuteNonQuery();
-            }
-
-            using (var createTable = new SqliteCommand(createDepartmentHeadTableCommand, connection))
-            {
-                createTable.ExecuteNonQuery();
-            }
-
-            using (var createTable = new SqliteCommand(createGuideTableCommand, connection))
-            {
-                createTable.ExecuteNonQuery();
-            }
-
-            using (var createTable = new SqliteCommand(createGuideInTourTableCommand, connection))
-            {
-                createTable.ExecuteNonQuery();
-            }
-
-            string insertDepartmentHeadDataCommand = @"
-                    INSERT OR IGNORE INTO DepartmentHead (Id, Name, QR) VALUES 
-                        (@Id, @Name, @Qr);";
-            
-            string insertGuideDataCommand = @"
-                    INSERT OR IGNORE INTO Guide (Id, Name, QR) VALUES 
-                        (@Id, @Name, @Qr);";
-
-                using (var insertData = new SqliteCommand(insertGuideDataCommand, connection))
-                {
-                    insertData.Parameters.AddWithValue("@Id", 1);
-                    insertData.Parameters.AddWithValue("@Name", "Casper");
-                    insertData.Parameters.AddWithValue("@Qr", "4892579");
-
-                    insertData.ExecuteNonQuery();
-                }
-            
-
-                using (var insertData = new SqliteCommand(insertDepartmentHeadDataCommand, connection))
-                {
-                    insertData.Parameters.AddWithValue("@Id", 1);
-                    insertData.Parameters.AddWithValue("@Name", "Frans");
-                    insertData.Parameters.AddWithValue("@Qr", "6457823");
-
-                    insertData.ExecuteNonQuery();
-                }
-            
-        }        
+             
         bool running = true;
 
         while (running)
@@ -131,33 +32,76 @@ class Program
                     Console.WriteLine("Scan your QR code:");
                     string qr = Console.ReadLine();
 
-                    visitor.QR = qr;
+                    bool accountCreated = visitor.AccCreated(qr);
 
-                    visitor.AccCreated(qr);
+                    string loginStatus;
 
-                    
-                    Console.WriteLine("Make reservation(E)\nMy reservations(M)\nCancel reservation(C)\nQuit(Q)");
-                    string option = Console.ReadLine();
-
-                    if (option.ToLower() == "e")
+                    if (accountCreated)
                     {
-                        Tours.ReservateTour(visitor);
-                    }
-                    else if (option.ToLower() == "m")
-                    {
-                        visitor.ViewReservationsMade(visitor.Id);
-                    }
-                    else if (option.ToLower() == "c")
-                    {
-                        visitor.CancelReservation(visitor);
-                    }
-                    else if (option.ToLower() == "q")
-                    {
-                        break;
+                        loginStatus = "Visitor";
+                        visitor.QR = qr;
                     }
                     else
                     {
-                        Console.WriteLine("Wrong input. Try again.");
+                        loginStatus = visitor.Login(qr);
+                    }
+
+                    if (loginStatus == "Visitor")
+                    {
+                        bool visitorRunning = true;
+                        while (visitorRunning)
+                        {
+                            Console.WriteLine("Make reservation(E)\nMy reservations(M)\nCancel reservation(C)\nQuit(Q)");
+                            string option = Console.ReadLine();
+
+                            if (option.ToLower() == "e")
+                            {
+                                Tours.ReservateTour(visitor);
+                            }
+                            else if (option.ToLower() == "m")
+                            {
+                                visitor.ViewReservationsMade(visitor.Id);
+                            }
+                            else if (option.ToLower() == "c")
+                            {
+                                visitor.CancelReservation(visitor);
+                            }
+                            else if (option.ToLower() == "q")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong input. Try again.");
+                            }
+                        }
+                    }
+                    else if (loginStatus == "Admin")
+                    {
+                        visitor.AdminMenu(2);
+                    }
+                    else if (loginStatus == "Guide")
+                    {
+                        bool guideRunning = true;
+
+                        while (guideRunning)
+                        {
+                            Console.WriteLine("My tours(M)\nQuit (Q)");
+                            string option = Console.ReadLine();
+
+                            if (option.ToLower() == "m")
+                            {
+                                Tours.guide.ViewTours("Casper");
+                            }
+                            else if (option.ToLower() == "q")
+                            {
+                                guideRunning = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong input. Try again.");
+                            }
+                        }
                     }
                 }
                 else if (choice.ToLower() == "q")
@@ -179,7 +123,7 @@ class Program
                 {
                     Console.WriteLine("Scan uw QR code:");
                     string qr = Console.ReadLine();
-
+                    
                     visitor.AccCreated(qr);
 
                     string loginStatus = visitor.Login(qr);
