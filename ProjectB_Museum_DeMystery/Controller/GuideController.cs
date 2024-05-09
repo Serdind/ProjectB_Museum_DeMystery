4d91a0f64ba0a4de984844cb13f8490a9d5c20ec
miraii03 <1076329@hr.nl>
2024-05-09 13:47:28 +0200

    update GuideController

--- ProjectB_Museum_DeMystery/Controller/GuideController.cs
+++ ProjectB_Museum_DeMystery/Controller/GuideController.cs
@@ -5,7 +5,7 @@ public static class GuideController
         int tourID = TourId.WhichTourId();
 
         Tour.OverviewVisitorsTour(tourID);
-    
+
         string option = GuideOptions.Options();
 
         if (option.ToLower() == "a")
@@ -22,6 +22,13 @@ public static class GuideController
         }
     }
 
+    public static void StartTour()
+    {
+        int tourID = TourId.WhichTourId();
+
+        Guide.StartTour(tourID);
+    }
+
     public static void OptionsGuide()
     {
         string option = ViewVisitors.Show();
@@ -30,9 +37,38 @@ public static class GuideController
         {
             ViewVisitorsTour();
         }
+        else if (option.ToLower() == "s")
+        {
+            StartTour();
+        }
         else
         {
             WrongInput.Show();
         }
+    }
+}
+public class GuideOptions : View
+{
+    public static string Options()
+    {
+        Console.WriteLine("Add visitor(A)\nRemove visitor(R)");
+        return ReadLineString();
+    }
+
+    public static void StartTour(GuidedTour tour)
+    {
+        string message = $"The tour has been started:" +
+                        $"Tour: {tour.Name}\n" +
+                        $"Date: {tour.Date.ToShortDateString()}\n" +
+                        $"Time: {tour.Date.ToString("HH:mm")}\n" +
+                        $"Language: {tour.Language}\n";
+
+        Console.WriteLine(message);
+    }
+
+    public static string ViewTours()
+    {
+        Console.WriteLine("My tours(M)");
+        return ReadLineString();
     }
 }
