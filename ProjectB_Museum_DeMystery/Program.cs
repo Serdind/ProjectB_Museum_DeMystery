817bba22c560bf30e194045e5fcee931cc97f234
Wishant010 <wishantbhajan@outlook.com>
2024-05-10 22:39:49 +0200

    program functie  added

--- ProjectB_Museum_DeMystery/Program.cs
+++ ProjectB_Museum_DeMystery/Program.cs
@@ -3,7 +3,11 @@
     public static IMuseum Museum = new RealMuseum();
     public static void Main()
     {
-        
+        string subdirectory = @"ProjectB\ProjectB_Museum_DeMystery\ProjectB_Museum_DeMystery";
+        string fileName = "unique_codes.json";
+        string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string filePath = Path.Combine(userDirectory, subdirectory, fileName);
+
         Tour.UpdateTours();
 
         Tour.OverviewTours(false);
@@ -11,7 +15,14 @@
         Tour.AddAdminToJSON();
 
         Tour.AddGuideToJSON();
-             
+
+        if (UniqueCodes.IsNewDay(filePath))
+        {
+            List<int> codes = UniqueCodes.GenerateUniqueCodes(50);
+
+            UniqueCodes.SaveCodesToJson(codes, filePath);
+        }
+
         bool running = true;
 
         while (running)
