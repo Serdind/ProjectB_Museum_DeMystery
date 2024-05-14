9f35f6ab073125618087f96b5853710d62fd36fe
miraii03 <1076329@hr.nl>
2024-05-14 14:05:20 +0200

    update ViewVisitors

--- ProjectB_Museum_DeMystery/View/ViewVisitors.cs
+++ ProjectB_Museum_DeMystery/View/ViewVisitors.cs
@@ -2,7 +2,7 @@ public class ViewVisitors : View
 {
     public static string Show()
     {
-        Console.WriteLine("View visitors(V)\nStart tour(S)");
+        Console.WriteLine("View visitors(V)\nStart tour(S)\nGo back(B)");
         return ReadLineString();
     }
 }
