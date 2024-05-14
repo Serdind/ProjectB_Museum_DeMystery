fb861ddc6251348fb215b13026b3b67a990f0315
miraii03 <1076329@hr.nl>
2024-05-14 14:13:36 +0200

    update UniqueCodes

--- ProjectB_Museum_DeMystery/Model/UniqueCodes.cs
+++ ProjectB_Museum_DeMystery/Model/UniqueCodes.cs
@@ -15,34 +15,4 @@ public class UniqueCodes
         }
         return new List<int>(codesSet);
     }
-
-    public static void SaveCodesToJson(List<int> codes, string fileName)
-    {
-        string json = JsonConvert.SerializeObject(codes, Formatting.Indented);
-        File.WriteAllText(fileName, json);
-    }
-
-    public static List<string> LoadUniqueCodesFromFile(string fileName)
-    {
-        if (File.Exists(fileName))
-        {
-            string json = File.ReadAllText(fileName);
-            return JsonConvert.DeserializeObject<List<string>>(json);
-        }
-        else
-        {
-            return new List<string>();
-        }
-    }
-
-    public static bool IsNewDay(string filePath)
-    {
-        if (File.Exists(filePath))
-        {
-            DateTime lastModified = File.GetLastWriteTime(filePath);
-
-            return lastModified.Date < DateTime.Today;
-        }
-        return true;
-    }
 }
