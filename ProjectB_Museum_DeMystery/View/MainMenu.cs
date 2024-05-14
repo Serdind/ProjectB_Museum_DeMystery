286b1c809888afca4216ecb323f33251c49ca3cd
Serdind <1055241@hr.nl>
2024-05-14 23:06:27 +0200

    main menu

--- ProjectB_Museum_DeMystery/View/MainMenu.cs
+++ ProjectB_Museum_DeMystery/View/MainMenu.cs
@@ -3,8 +3,8 @@ public class MainMenu : View
     public static string Menu()
     {
         Console.WriteLine("Welcome to Het Depot!");
-        Console.WriteLine("select language  /   selecteer taal");
-        Console.WriteLine("English(E)   /   Nederlands(N)");
+        Console.WriteLine("Select language");
+        Console.WriteLine("English(E)");
         return ReadLineString();
     }
 }
