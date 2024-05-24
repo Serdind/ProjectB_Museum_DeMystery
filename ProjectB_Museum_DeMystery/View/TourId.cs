dba3a46bb5d994d054238ccdceedd0ebe1b9449e
miraii03 <1076329@hr.nl>
2024-05-24 17:12:11 +0200

    update TourId

--- ProjectB_Museum_DeMystery/View/TourId.cs
+++ ProjectB_Museum_DeMystery/View/TourId.cs
@@ -2,7 +2,25 @@ public class TourId : View
 {
     public static int WhichTourId()
     {
-        Console.WriteLine("Which tour? (ID)");
-        return ReadLineInt();
+        while (true)
+        {
+            Console.Write("Enter tour ID:\n");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "b" || input.ToLower() == "back")
+            {
+                return -1;
+            }
+            else if (int.TryParse(input, out int tourID))
+            {
+                return tourID;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid numeric tour ID.");
+                Console.WriteLine("Press any key to continue...\n");
+                Console.ReadKey(true);
+            }
+        }
     }
 }
