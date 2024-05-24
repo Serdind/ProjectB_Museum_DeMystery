02fbf2efcef281121303436da9168eeaaedf1f4b
Wishant010 <wishantbhajan@outlook.com>
2024-05-24 17:03:26 +0200

    AdminOption tiny changes

--- ProjectB_Museum_DeMystery/View/AdminOptions.cs
+++ ProjectB_Museum_DeMystery/View/AdminOptions.cs
@@ -2,17 +2,28 @@ public class AdminOptions : View
 {
     public static string Options()
     {
-        Console.WriteLine("Overview tours(T)\nAdd tour (A)\nEdit tour (E)\nGo back (B)");
+        Console.WriteLine("Overview tours(T)\nAdd tour (A)\nEdit tour (E)\nLog out (L)");
         return ReadLineString();
     }
 
     public static void ReservationCancelled()
     {
         Console.WriteLine("Reservation cancelled successfully.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 
     public static void ReservationCancelDenied()
     {
         Console.WriteLine("Reservation cancellation cancelled.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
+    }
+
+    public static void BackOption()
+    {
+        Console.WriteLine("Insert (Back or B) if you want to go back");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 }
