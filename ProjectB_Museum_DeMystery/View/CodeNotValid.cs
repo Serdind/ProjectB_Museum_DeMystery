31280df6a883d9fadad1c80f8e63bd505c6e86da
Wishant010 <wishantbhajan@outlook.com>
2024-05-24 17:04:44 +0200

    CodeNotValid  added Writeline

--- ProjectB_Museum_DeMystery/View/CodeNotValid.cs
+++ ProjectB_Museum_DeMystery/View/CodeNotValid.cs
@@ -3,5 +3,7 @@ public class CodeNotValid : View
     public static void Show()
     {
         Console.WriteLine("Code is not valid.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 }
