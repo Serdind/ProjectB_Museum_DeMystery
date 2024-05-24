c280cb226c58275a07e41414fd5c56faed460b45
miraii03 <1076329@hr.nl>
2024-05-24 17:09:27 +0200

    update ReservationMenu

--- ProjectB_Museum_DeMystery/View/ReservationMenu.cs
+++ ProjectB_Museum_DeMystery/View/ReservationMenu.cs
@@ -2,14 +2,14 @@ public class ReservationMenu : View
 {
     public static string Menu(string qr, Visitor visitor)
     {
-        Console.WriteLine("Make reservation (R)");
+        Console.WriteLine("Make reservation(R)");
 
         if (visitor.ReservationMade(qr))
         {
-            Console.WriteLine("My reservations (M)");
-            Console.WriteLine("Cancel reservation (C)");
+            Console.WriteLine("My reservations(M)");
+            Console.WriteLine("Cancel reservation(C)");
         }
-        Console.WriteLine("Done (D)");
+        Console.WriteLine("Log out(L)");
         return ReadLineString();
     }
 }
