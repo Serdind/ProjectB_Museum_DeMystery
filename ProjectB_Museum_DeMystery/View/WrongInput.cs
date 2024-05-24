c8c84861b72c4068934e25d2cc17c438a9f4b353
miraii03 <1076329@hr.nl>
2024-05-24 17:15:41 +0200

    update WrongInput

--- ProjectB_Museum_DeMystery/View/WrongInput.cs
+++ ProjectB_Museum_DeMystery/View/WrongInput.cs
@@ -3,5 +3,7 @@ public class WrongInput
     public static void Show()
     {
         Console.WriteLine("Wrong input. Try again.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 }
