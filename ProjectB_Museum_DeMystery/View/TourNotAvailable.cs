91ec089b6f5d7db35414167b50fff57c08b2cc8b
miraii03 <1076329@hr.nl>
2024-05-24 17:13:59 +0200

    update TourNotAvailable

--- ProjectB_Museum_DeMystery/View/TourNotAvailable.cs
+++ ProjectB_Museum_DeMystery/View/TourNotAvailable.cs
@@ -3,5 +3,7 @@ public class TourNotAvailable : View
     public static void Show()
     {
         Console.WriteLine("Tour is not available.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 }
