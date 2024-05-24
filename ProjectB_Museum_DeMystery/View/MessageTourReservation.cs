abd63ee8d7b89b2715122e451f6d1f98f1877188
miraii03 <1076329@hr.nl>
2024-05-24 17:07:46 +0200

    update MessageTourReservation

--- ProjectB_Museum_DeMystery/View/MessageTourReservation.cs
+++ ProjectB_Museum_DeMystery/View/MessageTourReservation.cs
@@ -1,24 +1,30 @@
 public class MessageTourReservation
 {
-    public static string ShowMessage(GuidedTour tour)
+    public static void ShowMessage(GuidedTour tour)
     {
         string message = $"Reservation successful. You have reserved the following tour:\n" +
                         $"Date: {tour.Date.ToShortDateString()}\n" +
                         $"Time: {tour.Date.ToString("HH:mm")}\n" +
-                        $"Duration: 20 min\n" +
-                        $"Language: {tour.Language}\n";
+                        $"Duration: 40 min\n" +
+                        $"Language: {tour.Language}\n" +
+                        $"Guide: {tour.NameGuide}\n";
 
-        return message;
+        Console.WriteLine(message);
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 
     public static void ViewReservation(GuidedTour tour)
     {
         string message = $"Date: {tour.Date.ToShortDateString()}\n" +
                         $"Time: {tour.Date.ToString("HH:mm")}\n" +
-                        $"Duration: 20 min\n" +
-                        $"Language: {tour.Language}\n";
+                        $"Duration: 40 min\n" +
+                        $"Language: {tour.Language}\n" +
+                        $"Guide: {tour.NameGuide}\n";
 
         Console.WriteLine(message);
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 
     public static void ViewStart(GuidedTour tour)
@@ -26,13 +32,18 @@ public class MessageTourReservation
         string message = $"Tour has been started:\n" +
                         $"Date: {tour.Date.ToShortDateString()}\n" +
                         $"Time: {tour.Date.ToString("HH:mm")}\n" +
-                        $"Language: {tour.Language}\n";
+                        $"Language: {tour.Language}\n" +
+                        $"Guide: {tour.NameGuide}\n";
 
         Console.WriteLine(message);
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 
     public static void TourAdded()
     {
         Console.WriteLine("Tour succesfully added.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 }
