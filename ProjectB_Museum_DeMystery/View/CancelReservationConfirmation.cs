e37bc15fdf327907830160e8cebeff785c915d09
Wishant010 <wishantbhajan@outlook.com>
2024-05-24 17:04:04 +0200

    CancelReservation Added info

--- ProjectB_Museum_DeMystery/View/CancelReservationConfirmation.cs
+++ ProjectB_Museum_DeMystery/View/CancelReservationConfirmation.cs
@@ -2,17 +2,21 @@ public class CancelReservationConfirmation : View
 {
     public static string Options()
     {
-        Console.WriteLine("Are you sure you want to cancel your reservation? (Y/N)");
+        Console.WriteLine("Are you sure you want to cancel your reservation?\nYes or Y to comfirm\nNo or N to decline");
         return ReadLineString();
     }
 
     public static void ReservationCancelled()
     {
         Console.WriteLine("Reservation cancelled successfully.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
-    
+
     public static void ReservationCancelDenied()
     {
         Console.WriteLine("Reservation cancellation cancelled.");
+        Console.WriteLine("Press any key to continue...\n");
+        Console.ReadKey(true);
     }
 }
