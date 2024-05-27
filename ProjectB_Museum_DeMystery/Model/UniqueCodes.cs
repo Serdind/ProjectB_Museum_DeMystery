0c0a679a8b965fd0b8bffd85ee5755853acf222c
Serdind <1055241@hr.nl>
2024-05-27 12:55:20 +0200

    uniquecodes change

--- ProjectB_Museum_DeMystery/Model/UniqueCodes.cs
+++ ProjectB_Museum_DeMystery/Model/UniqueCodes.cs
@@ -6,13 +6,44 @@ public class UniqueCodes
 
     public List<int> GenerateUniqueCodes(int count)
     {
+        string subdirectory = @"ProjectB\ProjectB_Museum_DeMystery\ProjectB_Museum_DeMystery";
+        string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        string adminsFilePath = Path.Combine(userDirectory, subdirectory, "admins.json");
+        string guidesFilePath = Path.Combine(userDirectory, subdirectory, "guides.json");
+
+        HashSet<int> existingCodes = new HashSet<int>();
+        if (File.Exists(adminsFilePath))
+        {
+            string json = File.ReadAllText(adminsFilePath);
+            var admins = JsonConvert.DeserializeObject<List<DepartmentHead>>(json);
+            foreach (var admin in admins)
+            {
+                existingCodes.Add(Convert.ToInt32(admin.QR));
+            }
+        }
+
+        if (File.Exists(guidesFilePath))
+        {
+            string json1 = File.ReadAllText(guidesFilePath);
+            var guides = JsonConvert.DeserializeObject<List<Guide>>(json1);
+            foreach (var guide in guides)
+            {
+                existingCodes.Add(Convert.ToInt32(guide.QR));
+            }
+        }
+
         HashSet<int> codesSet = new HashSet<int>();
 
         while (codesSet.Count < count)
         {
             int randomNumber = random.Next(100000, 1000000);
-            codesSet.Add(randomNumber);
+            if (!existingCodes.Contains(randomNumber))
+            {
+                codesSet.Add(randomNumber);
+            }
         }
+
         return new List<int>(codesSet);
     }
 
